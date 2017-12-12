6b52b99e0be73d7e6123bceb5c0a4adad4200074
v_mnogoletov <v_mnogoletov@mail.ru>
2017-12-12 13:57:38 +0700

    Temlate Build

--- PW_BOT_MVVM_GUI/View/MainWindow.xaml.cs
+++ PW_BOT_MVVM_GUI/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PW_BOT_MVVM_GUI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PW_BOT_MVVM_GUI
+namespace PW_BOT_MVVM_GUI.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +24,7 @@ namespace PW_BOT_MVVM_GUI
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
         }
     }
 }
---
+++ PW_BOT_MVVM_GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using PW_BOT_MVVM_GUI.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW_BOT_MVVM_GUI.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        RelayCommand loadedCommand;
+        RelayCommand onClosingCommand;
+
+        //команда выполняемая при загрузке главного окна
+        public RelayCommand LoadedCommand
+        {
+            get
+            {
+                return loadedCommand ??
+                    (loadedCommand = new RelayCommand(
+                        (o) => {
+                            try
+                            {
+                                Debug.WriteLine("OK");
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                            }
+
+                        }));
+            }
+        }
+        //команда выполняемая при выходе
+        public RelayCommand OnClosingCommand
+        {
+            get
+            {
+                return onClosingCommand ??
+                    (onClosingCommand = new RelayCommand(
+                        (o) => {
+                            try
+                            {
+                                Properties.Settings.Default.Save();
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                            }
+
+                        }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
